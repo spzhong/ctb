ca4e08162a0f37312f96d712647aa0365c3be382
宋培众 <spzhongwin@126.com>
2019-02-15 14:18:54 +0800

    commit

--- ctb/application/user/UserInfo.py
+++ ctb/application/user/UserInfo.py
@@ -67,7 +67,7 @@ def adminGetAllUsers(request):
     list = []
     for oneuser in userList:
         list.append({"id":oneuser.id,"openId":oneuser.openId,"trueName":oneuser.trueName,"name":oneuser.name,"address":oneuser.address,"phone":oneuser.phone,"role":oneuser.role})
-    callBackDict['totalNum'] = user.objects.all()
+    callBackDict['totalNum'] = user.objects.all().count()
     return Comm.callBackSuccess(callBackDict, 1, list)
 
 
