3803f7396c731af8dde69cdc6ab48d73436b3485
宋培众 <spzhongwin@126.com>
2019-05-30 19:11:05 +0800

    commit

--- ctb/crontab.py
+++ ctb/crontab.py
@@ -9,8 +9,8 @@ import urllib
 # 执行的任务
 def doTask():
     print '执行了'
-    f = urllib.urlopen('http://appid.985-985.com:8088/getAppConfig.php?appid=chenspeed2')
-    print f.read().decode('utf-8')
+    #f = urllib.urlopen('http://appid.985-985.com:8088/getAppConfig.php?appid=chenspeed2')
+    #print f.read().decode('utf-8')
 
 
 
